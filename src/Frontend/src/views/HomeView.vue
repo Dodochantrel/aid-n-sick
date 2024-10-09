cbf978e7f77e1cdd84c8f494d472506675efb56f
mateo <mateobatistagomes@gmail.com>
2024-10-09 16:49:22 +0200

    separation body + css de base

--- src/Frontend/src/views/HomeView.vue
+++ src/Frontend/src/views/HomeView.vue
@@ -3,7 +3,13 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
+  <header>
+    Header
+  </header>
   <main>
-    <TheWelcome />
+    <h1>Body</h1>
   </main>
+  <footer>
+    Footer
+  </footer>
 </template>
