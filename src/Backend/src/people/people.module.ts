8c1d24ff442273f1836309f9e8f479230212ba30
Chantrel.D <dorian.chantrel@hotmail.fr>
2024-11-04 14:51:39 +0100

    free ugo est chiant

--- src/Backend/src/people/people.module.ts
+++ src/Backend/src/people/people.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { PeopleController } from './people.controller';
+import { People } from './people.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([People])],
   controllers: [PeopleController],
   providers: [PeopleService],
 })
