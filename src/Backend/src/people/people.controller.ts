1c9b60491c39a8977cd5cbe7684a19b543df7c1b
Chantrel.D <dorian.chantrel@hotmail.fr>
2024-11-04 14:49:54 +0100

    free ugo est chiant

--- src/Backend/src/people/people.controller.ts
+++ src/Backend/src/people/people.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { PeopleService } from './people.service';
+import { People } from './people.entity';
 
 @Controller('people')
 export class PeopleController {
   constructor(private readonly peopleService: PeopleService) {}
 
   @Get()
-  getAll(): string {
+  getAll(): Promise<People[]> {
     return this.peopleService.getAll();
   }
 }
